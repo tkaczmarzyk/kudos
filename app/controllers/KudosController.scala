cfcf62dace786cf431e760cee59c9008c8855bba
tkaczmarzyk <tomasz+github@kaczmarzyk.net>
2013-04-19 19:24:22 +0200

    find by kudos by id

--- app/controllers/KudosController.scala
+++ app/controllers/KudosController.scala
@@ -7,15 +7,30 @@ import play.api.mvc.Controller
 import models.{Kudos, Kudoses}
 import global.Global._
 import play.api.libs.json.Json
+import views.html.defaultpages.badRequest
 
 
 object KudosController extends Controller {
-
+  
   def list = Action {
     val kudos = database.withSession {
-      (for (b <- Kudoses) yield b).list
+      (for (k <- Kudoses) yield k).list
     }
     
-    Ok(kudos.toString)
+//    Ok(kudos.foldLeft(Json.obj())(
+//        (acc, kudos) => acc.obj("bl" -> "df")))
+    
+    Ok(Json.obj("test" -> kudos(0).name))
+  }
+  
+  def findById(id: Int) = Action {
+    val found = database.withSession {
+      (for (k <- Kudoses if k.id === id) yield k).firstOption
+    }
+    
+    found match {
+      case Some(kudos) => Ok(kudos.toString)
+      case None => NotFound("no such element")
+    }
   }
 }
