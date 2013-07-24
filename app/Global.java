071be7db7c4652ff41cd4f5e99999bf7facaca96
Michal Jankowski <jankowski.michal.tomasz@gmail.com>
2013-07-24 08:49:57 +0200

    removed initial kudos and person, minor view enhancements

--- app/Global.java
+++ app/Global.java
@@ -12,24 +12,6 @@ public class Global extends GlobalSettings {
     @Override
     public void onStart(Application application) {
         this.application = application;
-        Kudos kudos = createKudos("First Kudos","for app creation",createDummyPerson());
-        kudos.save();
     }
 
-    private Kudos createKudos(String reason, String details, Person person) {
-        Kudos k = new Kudos();
-        k.reason = reason;
-        k.details = details;
-        k.target = person;
-        return k;
-    }
-
-    private Person createDummyPerson(){
-        Person p = new Person();
-        p.name = "Dummy";
-        p.smallPhotoUrl = "http://cohesiva.com/images/people_thumbs/tomekk_alt.jpg";
-        p.bigPhotoUrl = "http://cohesiva.com/images/people_thumbs/tomekk_alt.jpg";
-        p.save();
-        return p;
-    }
 }
